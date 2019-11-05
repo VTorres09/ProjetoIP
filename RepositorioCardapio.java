9ed446629d7cd48473ba30f9b46178ee15177a00
Vinicius Torres de Macedo <vtm@cin.ufpe.br>
2019-11-05 00:24:17 -0300

    Delete RepositorioCardapio.java

--- RepositorioCardapio.java
+++
@@ -1,7 +0,0 @@
-
-public interface RepositorioCardapio {
-	void adicionarPrato(Prato prato);
-	void removerPrato(Prato prato);
-	String listarPrato(RepositorioCardapio repositorioCardapio);
-	boolean existePrato(String nomePrato);
-}
