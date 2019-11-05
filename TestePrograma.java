226897b29ab8967038c780e3effb7d94357ae297
Vinicius Torres de Macedo <vtm@cin.ufpe.br>
2019-11-05 19:40:54 -0300

    Update TestePrograma.java

--- TestePrograma.java
+++ TestePrograma.java
@@ -2,14 +2,32 @@ import java.util.*;
 public class TestePrograma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		Prato cocacola = new Prato("Coca cola", 6.00, "Cafeina");
-		Prato fanta = new Prato("Fanta", 6.00, "Laranja");
-		RepositorioCardapioLista lista =  new RepositorioCardapioLista();
+		
+		//cadastro ingredientes		
+		Ingrediente molhoTomate = new Ingrediente("Molho de Tomate", 10, "Caixas", "Hellmans", 5.50 , "01/01/2020");
+		Ingrediente macarrao = new Ingrediente("Spaghetti", 3, "Pacotes", "Bom Preco", 8.90 , "21/03/2020");
+		Ingrediente cocaLata = new Ingrediente("Coca Cola", 10, "Lata", "", 3.00, "");
+		
+		//cadastro da lista de ingredientes do prato
+		Ingrediente[] ingredientesMacarronada = {molhoTomate, macarrao};
+		Ingrediente[] cocaLataa = {cocaLata};
+		
+		//criacao do armazenamento de cardapio
+		RepositorioCardapioArray lista =  new RepositorioCardapioArray(90);
+		
+		//criacao dos pratos
+		Prato macarronada = new Prato("Macarronada", 24.00, ingredientesMacarronada);
+		Prato cocacola = new Prato("Coca Cola", 6.00, cocaLataa);	
+		
+		//criacao cardapio
 		Cardapio cardapio = new Cardapio(lista);
+		
+		//cadastro dos pratos em cardapio
 		cardapio.cadastrar(cocacola);
-		cardapio.cadastrar(fanta);
+		cardapio.cadastrar(macarronada);
+		
+		//listar itens cardapio
 		System.out.print(cardapio.listar());
 	}
 
