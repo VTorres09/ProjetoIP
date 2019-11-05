ded0c27b7dbdfd28eae9cad119abceb2130c9104
Vinicius Torres de Macedo <vtm@cin.ufpe.br>
2019-11-05 19:39:03 -0300

    Update RepositorioCardapioArray.java

--- Cardápio/RepositorioCardapioArray.java
+++ Cardápio/RepositorioCardapioArray.java
@@ -2,8 +2,8 @@
 public class RepositorioCardapioArray implements RepositorioCardapio {
 	private Prato[] arrayPratos;
 	private int cont;
-	
-	RepositorioCardapioArray(int tamanho){
+
+	RepositorioCardapioArray(int tamanho) {
 		this.arrayPratos = new Prato[tamanho];
 		this.cont = 0;
 	}
@@ -14,10 +14,10 @@ public class RepositorioCardapioArray implements RepositorioCardapio {
 	}
 
 	public void removerPrato(Prato prato) {
-		for(int i = 0; i<cont; i++) {
-			if(arrayPratos[i] == prato) {
-				for(int k = i+1; k<cont; k++) {
-					arrayPratos[k-1] = arrayPratos[k];
+		for (int i = 0; i < cont; i++) {
+			if (arrayPratos[i] == prato) {
+				for (int k = i + 1; k < cont; k++) {
+					arrayPratos[k - 1] = arrayPratos[k];
 				}
 			}
 		}
@@ -26,27 +26,45 @@ public class RepositorioCardapioArray implements RepositorioCardapio {
 	}
 
 	public String listarPrato() {
-		String retorno = "-------- Cardápio --------" + "\n";
-		for (int i = 0; i < cont; i++) {			
-			retorno += "Nome: " + arrayPratos[i].getNome() + "\n" + "Preço: " + arrayPratos[i].getPreco() + "\n"
-					+ "Ingredientes: " + arrayPratos[i].getIngredientes() + "\n" + "--------------------------" + "\n";	
+		String retorno = "-------- CardÃ¡pio --------" + "\n";
+		for (int i = 0; i < cont; i++) {
+			retorno += "Nome: " + arrayPratos[i].getNome() + "\n" + "PreÃ§o: " + arrayPratos[i].getPreco() + "\n"
+					+ "Ingredientes: " + listarIngredientes(arrayPratos[i]) + "\n" + "--------------------------" + "\n";
+		}
+		return retorno;
+	}
+
+	public String listarIngredientes(Prato prato) {
+		String retorno = "";
+		for (int x = 0; x < cont; x++) {
+			if (arrayPratos[x] == prato) {
+				for (int i = 0; i < arrayPratos[x].getIngredientes().length; i++) {
+					if (i == arrayPratos[x].getIngredientes().length - 1) {
+						retorno += arrayPratos[x].getIngredientes()[i].getNome() + ".";
+					} else {
+						retorno += arrayPratos[x].getIngredientes()[i].getNome() + ", ";
+					}
+
+				}
+
+			}
+
 		}
 		return retorno;
 	}
 
 	public boolean existePrato(String nomePrato) {
 		boolean retorno = false;
-		for(int i = 0; i<cont; i++) {
-			if(arrayPratos[i].getNome().equals(nomePrato)) {
+		for (int i = 0; i < cont; i++) {
+			if (arrayPratos[i].getNome().equals(nomePrato)) {
 				retorno = true;
 			}
 		}
 		return retorno;
 	}
-	
+
 	public void atualizarPrato(Prato prato) {
-		
+
 	}
 
-
-} 
+}
