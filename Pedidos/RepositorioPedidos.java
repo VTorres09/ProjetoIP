164d444c664ba0912c7bce334d46c01dc41ad09d
Vinicius Torres de Macedo <vtm@cin.ufpe.br>
2019-11-11 16:27:48 -0300

    Delete RepositorioPedidos.java

--- Pedidos/RepositorioPedidos.java
+++
@@ -1,7 +0,0 @@
-public interface RepositorioPedidos {
-	void inserir(Pedidos pedido);
-	void remover(Pedidos pedido);
-	void atualizar(Pedidos pedido, int novaQuantidade);
-	String listar(); //metodo "procurar"
-	boolean verificar(Pedidos pedido);
-}
