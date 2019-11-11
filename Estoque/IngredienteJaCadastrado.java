b9758b23e204bfa8739e061ba3e97fd160e002d6
Felipe Muniz de Araujo <fma2@ug3c15.windows.cin.ufpe.br>
2019-11-11 18:04:22 -0300

    alterando mesa

--- Estoque/IngredienteJaCadastrado.java
+++
@@ -1,9 +0,0 @@
-package Estoque;
-
-public class IngredienteJaCadastrado extends Exception{
-
-	public IngredienteJaCadastrado(){
-		super("Esse ingrediente já está cadastrado no estoque");
-		//Informacoes do ingrediente ja existente
-	}
-}
